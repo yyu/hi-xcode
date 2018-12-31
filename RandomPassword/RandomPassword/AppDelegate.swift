174f155ec92dad4235ad76c63dc3dac7c870601c
y² <yy@yuyuan.org>
2018-12-31 11:18:33 -0800

    [RandomPassword] created new

---
+++ RandomPassword/RandomPassword/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  RandomPassword
+//
+//  Created by Yuan Yu on 12/31/18.
+//  Copyright © 2018 Y². All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
