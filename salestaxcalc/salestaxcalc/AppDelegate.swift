2d041d969717b91c6c2c8108317bb1d95bdce2a6
y² <forresty@amazon.com>
2018-12-31 03:01:54 -0800

    [SalesTaxCalc] created new

---
+++ salestaxcalc/salestaxcalc/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  salestaxcalc
+//
+//  Created by Yu, Yuan (forresty) on 12/31/18.
+//  Copyright © 2018 Yu, Yuan (forresty). All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ salestaxcalc/salestaxcalc/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  salestaxcalc
+//
+//  Created by Yu, Yuan (forresty) on 12/31/18.
+//  Copyright © 2018 Yu, Yuan (forresty). All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
