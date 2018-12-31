66e222fc695bbfa81142734d5f17a650642cd897
y² <yy@yuyuan.org>
2018-12-31 11:27:40 -0800

    [RandomPassword][MainWindowController] created new

---
+++ RandomPassword/README.md
@@ -0,0 +1,5 @@
+## reading *Cocoa Programming for OS X: The Big Nerd Ranch Guide*
+
+https://learning.oreilly.com/library/view/cocoa-programming-for/9780134077130/
+
+https://learning.oreilly.com/library/view/cocoa-programming-for/9780134077130/ch01s02.html
---
+++ RandomPassword/RandomPassword/MainWindowController.swift
@@ -0,0 +1,19 @@
+//
+//  MainWindowController.swift
+//  RandomPassword
+//
+//  Created by Yuan Yu on 12/31/18.
+//  Copyright © 2018 Y². All rights reserved.
+//
+
+import Cocoa
+
+class MainWindowController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+    
+}
