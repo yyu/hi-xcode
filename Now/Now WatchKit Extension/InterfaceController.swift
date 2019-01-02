3f48a50caa98d44c3155d3b9ba25d9aa4fb9de4e
y² <yy@yuyuan.org>
2019-01-01 17:17:21 -0800

    [Now] watch app

---
+++ Now/Now WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  Now WatchKit Extension
+//
+//  Created by Y² on 1/1/19.
+//  Copyright © 2019 Y². All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ Now/Now/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Now
+//
+//  Created by Y² on 1/1/19.
+//  Copyright © 2019 Y². All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
